66df4f326e232c54701196f83d0d92ee4450ef81
Sungmin Woo <woos8899@gmail.com>
2020-10-29 00:35:06 +0900

    New constants

--- src/utils/konst.ts
+++ src/utils/konst.ts
@@ -5,6 +5,8 @@ export const KEY_ACC_ID_LIST = "acc_id_list";
 export const KEY_COLUMN_LIST = "column_list";
 export const KEY_METADATA = "metadata";
 
+export const KEY_SIMILARITY_IDENTITY = "simil_identity";
+export const KEY_SIMILARITY_BIT_SCORE = "simil_bit_score";
 export const KEY_HOW_MANY = "how_many";
 
 export const KEY_ERROR_CODE = "error_code";

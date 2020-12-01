e54d7b6327e6c344ced4a97f5460a14b8f7bd015
hayunyun <hayunyun@naver.com>
2020-12-02 06:40:52 +0900

    Visualize Num_cases_per_country

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,65 +1,61 @@
 import * as React from 'react';
 import { Header } from 'semantic-ui-react';
-import { Map, GoogleApiWrapper, IMapProps, Circle } from 'google-maps-react';
+import { Map, GoogleApiWrapper, IMapProps, Circle, InfoWindow } from 'google-maps-react';
 // import GoogleMapReact from 'google-map-react';
 import '../major_elements/index.js';
 
+import * as cst from "../utils/konst";
 import i18n from '../i18n';
-// import { GoogleMapProps } from '@react-google-maps/api';
-
-// const AnyReactComponent = ({ text }) => <div>{text}</div>;
- 
-// export class SimpleMap extends React.Component<any> {
-//   constructor(props){
-//     super(props);
-//   }
-
-//   static defaultProps = {
-//     center: {
-//       lat: 59.95,
-//       lng: 30.33
-//     },
-//     zoom: 1
-//   };
- 
-//   render() {
-//     return (
-//       <div>
-//         <Header as='h1' dividing>{i18n.t("map")}</Header>
-//         <div style={{ height: '72vh', width: '90%' }}>
-//           <GoogleMapReact
-//             bootstrapURLKeys={{ 
-//                 key: 'AIzaSyB_bPUXCOGN-mCxgCfllV7JygB8E_huPEg',
-//                 language: 'ko',
-//                 region: 'kr'}}
-//             defaultCenter={this.props.center}
-//             defaultZoom={this.props.zoom}
-//             yesIWantToUseGoogleMapApiInternals
-//           >
-//             <AnyReactComponent
-//               lat={59.955413}
-//               lng={30.337844}
-//               text="My Marker"
-//             />
-//           </GoogleMapReact>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
- 
-// export default SimpleMap;
+import * as clt from "../utils/client";
 
 
-export class MapContainer extends React.Component<IMapProps> {
+interface MapContainerState {
+  zoom_level: number;
+  info_box_lat: number,
+  info_box_lng: number,
+
+  country_list:any,
+
+  map: any,
+}
+
+export class MapContainer extends React.Component<IMapProps, MapContainerState> {
   
   constructor(props) {
     super(props);
 
     this.state = {
-      isLoading: false,
+      zoom_level: 2,
+      info_box_lat: 35,
+      info_box_lng: 155,
+
+      country_list: [],
+
+      map: null,
     }
-  }
+
+    this.on_zoom_changed = this.on_zoom_changed.bind(this);
+
+    clt.num_cases_per_country()
+      .then(response => {
+        const payload = response.data
+        const error_code = payload[cst.KEY_ERROR_CODE]
+
+        if (0 == error_code) {
+            this.setState({
+                country_list: payload[cst.KEY_RESULT]
+            })
+        }
+        else {
+            const err_msg = payload[cst.KEY_ERROR_TEXT];
+            console.log(err_msg);
+        }
+      })
+      .catch(err => {
+          console.log(err);
+      })
+    };
+  
   
   render() {
     const mapStyles = {
@@ -67,56 +63,25 @@ export class MapContainer extends React.Component<IMapProps> {
       height: '90%',
       textAlign: 'center'
     };
-    
-    const coords = { lat: 35, lng: 155 };
 
-    interface Country {
-      center: google.maps.LatLngLiteral;
-      case: number;
-    }
-
-    const citymap: Record<string, Country> = {
-      chicago: {
-        center: { lat: 41.878, lng: -87.629 },
-        case: 2714856,
-      },
-      newyork: {
-        center: { lat: 40.714, lng: -74.005 },
-        case: 8405837,
-      },
-      losangeles: {
-        center: { lat: 34.052, lng: -118.243 },
-        case: 3857799,
-      },
-      vancouver: {
-        center: { lat: 49.25, lng: -123.1 },
-        case: 603502,
-      },
-    };
-
+    const ctryinfo = this.state.country_list
     const mapinfo = [];
-    for (const city in citymap) {
-      // Add the circle for this city to the map.
-      // const cityCircle = new Circle({
-      //   strokeColor: "#FF0000",
-      //   strokeOpacity: 0.8,
-      //   strokeWeight: 2,
-      //   fillColor: "#FF0000",
-      //   fillOpacity: 0.35,
-      //   center: citymap[city].center,
-      //   radius: Math.sqrt(citymap[city].case) * 100,
-      // })
-      mapinfo.push(<Circle
-        strokeColor= "#FF0000"
-        trokeOpacity= {0.8}
-        strokeWeight= {2}
-        fillColor= "#FF0000"
-        fillOpacity= {0.35}
-        center= {citymap[city].center}
-        radius= {Math.sqrt(citymap[city].case) * 100}
-      />)
+    for (const country in ctryinfo) {
+      if (ctryinfo[country]['center']!=null){
+        mapinfo.push(
+          <Circle
+            strokeColor= "#FF0000"
+            trokeOpacity= {0.8}
+            strokeWeight= {2}
+            fillColor= "#FF0000"
+            fillOpacity= {0.35}
+            center= {ctryinfo[country]['center']}
+            radius= {ctryinfo[country]['num_cases'] * 30}
+          />
+        )
+      } else continue;
     }
-
+    
     return (
       <div>
         <Header as='h1' dividing>{i18n.t("map")}</Header>
@@ -128,54 +93,50 @@ export class MapContainer extends React.Component<IMapProps> {
               // centerAroundCurrentLocation={true}
               style={mapStyles}
               initialCenter={{ lat: 35, lng: 155 }}
+
+              onZoomChanged={this.on_zoom_changed}
             >
-              {/* <Marker position={{ lat: 0, lng: 0}} /> */}
               
               {mapinfo}
+
               {/* <Circle
-                strokeColor= "#FF0000"
-                trokeOpacity= {0.8}
-                strokeWeight= {2}
-                fillColor= "#FF0000"
-                fillOpacity= {0.35}
-                center= {{ lat: 40.714, lng: -74.005 }}
-                radius= {Math.sqrt(8405837) * 100}
-              />
-
-              <Circle
-                strokeColor= "#FF0000"
-                trokeOpacity= {0.8}
-                strokeWeight= {2}
-                fillColor= "#FF0000"
-                fillOpacity= {0.35}
-                center= {{ lat: 49.25, lng: -123.1 }}
-                radius= {Math.sqrt(603502) * 100}
+                radius={1200000 / (this.state.zoom_level + 1)}
+                center={{ lat: 35, lng: 155 }}
+                onMouseover={() => console.log('mouse enter')}
+                onClick={() => console.log('click')}
+                onMouseout={() => console.log('mouse out')}
+                strokeColor='transparent'
+                strokeOpacity={0}
+                strokeWeight={5}
+                fillColor='#FF0000'
+                fillOpacity={0.3}
               /> */}
-              {/* {mapinfo.map(i=> {i})} */}
-              
-{/* 
-              <Circle 
-                center={coords}
-                strokeColor= "#FF0000"
-                strokeOpacity= {0.8}
-                strokeWeight= {2}
-                fillColor="#FF0000"
-                fillOpacity= {0.35}
-                radius={5000000}
-              /> */}
-
-              {/* <InfoWindow>
-                <div>
-                  <h1>test</h1>
-                </div>
+              {/* <InfoWindow
+                marker={null}
+                google={window.google}
+                map={this.state.map}
+                position={{ lat: this.state.info_box_lat, lng: this.state.info_box_lng }}
+                visible={true}
+              >
+                  <p>305 cases</p>
               </InfoWindow> */}
-              
+
             </Map>
         </div>
         
       </div>
     );
   }
+
+  private on_zoom_changed(mapProps?: IMapProps, map?: google.maps.Map, event?) {
+    const zoom_level = map.getZoom();
+  
+    this.setState({
+      zoom_level: zoom_level,
+      map: map,
+    });
+  }
+  
 }
 
 

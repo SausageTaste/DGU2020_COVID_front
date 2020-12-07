35e4e638b2d82b8a62041a1de07065265d2ec23b
woose28 <dldntmdsla@naver.com>
2020-12-07 23:32:34 +0900

    Add description of function and example sequence

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,7 +2,10 @@ import * as React from 'react';
 import { Header, Item, Segment } from 'semantic-ui-react';
 
 import i18n from './../i18n';
-
+import Describe_single_seq from './describe_single_seq';
+import Describe_two_seq from './describe_two_seq';
+import Describe_seq_list_in_db from './describe_seq_list_in_db';
+import Example_seq from './example_seq';
 
 export class HomePage extends React.Component<{}, {}> {
 
@@ -20,7 +23,10 @@ export class HomePage extends React.Component<{}, {}> {
                     <Item>
                         <Item.Content>
                             <Item.Header>{i18n.t("single_seq_query")}</Item.Header>
-                            <Item.Description>{i18n.t("single_seq_query_description")}</Item.Description>
+                            <Item.Description>
+                                <p>{i18n.t("single_seq_query_description")}</p>
+                                <Describe_single_seq/>
+                            </Item.Description>
                         </Item.Content>
                     </Item>
                     <Item>
@@ -29,6 +35,7 @@ export class HomePage extends React.Component<{}, {}> {
                             <Item.Description>
                                 <p>{i18n.t("two_seq_comp_description")}</p>
                                 <p>{i18n.t("two_seq_comp_description_2")}</p>
+                                <Describe_two_seq/>
                             </Item.Description>
                         </Item.Content>
                     </Item>
@@ -38,6 +45,7 @@ export class HomePage extends React.Component<{}, {}> {
                             <Item.Description>
                                 <p>{i18n.t("seq_list_in_db_description")}</p>
                                 <p>{i18n.t("seq_list_in_db_description_2")}</p>
+                                <Describe_seq_list_in_db/>
                             </Item.Description>
                         </Item.Content>
                     </Item>
@@ -50,6 +58,7 @@ export class HomePage extends React.Component<{}, {}> {
                 </Item.Group>
 
             </div>
+            <Example_seq/>
             <div style={{marginTop:140, bottom:0, textAlign:'center'}}>
                 <Header as="h6">Developed by 24.0</Header>
                 <p>윤건식 우성민 이우승 하윤영</p>
